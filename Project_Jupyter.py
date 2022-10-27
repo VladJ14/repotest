60c14e45e69993bb6a8170043ef8611bc8b67d21
VladJ14 <116109687+VladJ14@users.noreply.github.com>
2022-10-26 20:00:49 -0400

    Add files via upload

---
+++ Project_Jupyter.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Vladimir Tapia**
+# Data Scientist
+
+# *I am interested in data science because it is useful and brings advantages for many applications and sectors. I studied industrial engineering, and I was able to observe the importance of data science in any type of industry.*
+
+# ### The following code gives the result of the multiplication of two numbers x=2 and y=3.
+
+# In[1]:
+
+
+x = 2
+y = 3
+print ( x * y )
+
+
+# There´s differents types of browsers for example:
+# * [Google](https://www.google.com)
+# * [Bing](https://www.bing.com)
+# * [DuckDuckGo](https://duckduckgo.com)
+# ***
+# Browsers where anyone can find different types of resources like web pages, images, videos, and others. For example a logo:
+# ![alt text][logo]
+# 
+# [logo]: https://st2.depositphotos.com/1024516/7327/v/450/depositphotos_73275143-stock-illustration-vector-logo-for-letter-v.jpg
+
+# In[ ]:
+
+
+
+
